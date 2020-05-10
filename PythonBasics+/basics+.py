6ef75bebbd03a8be43c82712113513f9bdacb65a
dmosyan <dmosyan@gmail.com>
2020-05-10 22:16:30 +0400

    added some more methods

--- PythonBasics+/basics+.py
+++ PythonBasics+/basics+.py
@@ -11,3 +11,31 @@ print(basket) #[1, 2, 3, 4, 5, 100]
 
 basket.insert(2, 200)
 print(basket) # adding 200 in the 2nd index
+
+basket.extend([100,1001])
+new_list2 = basket
+print(basket)
+
+#removing
+basket.pop() #pops the last item
+print(basket)
+
+#basket.pop(0) removing 0 index item
+basket.remove(100)
+print(basket)
+
+basket.clear() #clears everything from list
+print(basket)
+
+print(basket.index(3)) #check the index of value 3
+print(3 in basket) #True
+print(basket.count(3)) #how many times 3 appears in the list
+
+basket = ['a', 'x','b', 'c', 'd', 'e', 'd' ]
+print(sorted(basket))
+basket.sort()
+print(basket)
+#both moethonds prints the same ['a', 'b', 'c', 'd', 'd', 'e', 'x']
+#sort does not produce a new list, but sorted produce!!
+basket.reverse() #switches the indexies
+print(basket)
